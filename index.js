ba1fb05569cedc0a0c284e62948f30fdd058e3c4
Yanchuk Andrey <orpiy@mail.ru>
2024-12-07 05:53:42 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+import { sayPrimeOrNot } from "./exported-functions.js";
+/*-----------------------------------------------------*/
+sayPrimeOrNot(5);
+sayPrimeOrNot(4);
+sayPrimeOrNot(37);
+sayPrimeOrNot(3);
+/*-----------------------------------------------------*/
