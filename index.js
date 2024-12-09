a7e801bc1b355f8e0d05075a5a667611dcf2fb42
Yanchuk Andrey <orpiy@mail.ru>
2024-12-09 12:30:54 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // index.js
-import { sayPrimeOrNot } from "./exported-functions.js";
+import { sayPrimeOrNot, expect, average } from "./exported-functions.js";
 /*-----------------------------------------------------*/
 sayPrimeOrNot(5);
 sayPrimeOrNot(4);
 sayPrimeOrNot(37);
 sayPrimeOrNot(3);
 /*-----------------------------------------------------*/
+console.log(average(0)); // 0
+console.log(average(0, 10)); // 5
+console.log(average(-3, 4, 2, 10)); // 3.25
+console.log(average()); // null
+/*-----------------------------------------------------*/
+console.log(expect(5).toBe(5));
+console.log(expect(5).toBe(null));
+console.log(expect(5).notToBe(null));
