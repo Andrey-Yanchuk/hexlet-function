41351a1b87e41187e28c548ebbe15b8682238c4a
Yanchuk Andrey <orpiy@mail.ru>
2024-12-12 21:11:32 +0700

    update index.js

--- index.js
+++ index.js
@@ -100,3 +100,12 @@ console.log(getGirlFriends(friendsUserList));
 //   { name: 'Keit', gender: 'female' },
 // ];
 /*-----------------------------------------------------*/
+const students = [
+  { name: 'Tirion', class: 'B', mark: 3 },
+  { name: 'Keit', class: 'A', mark: 3 },
+  { name: 'Ramsey', class: 'A', mark: 4 },
+];
+console.log(groupBy([], ''));
+console.log(groupBy(students, 'mark'));
+console.log(groupBy(students, 'class'));
+/*-----------------------------------------------------*/
